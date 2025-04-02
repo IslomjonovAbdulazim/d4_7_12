aae433e40f3824eaca8eea75b7194e6126183e33
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-02 17:29:12 +0500

    imp

--- lib/models/contact_model.dart
+++ lib/models/contact_model.dart
@@ -1,1 +1,25 @@
-class ContactModel {}
+class ContactModel {
+  late String id;
+  late String name;
+  late String tel;
+
+  ContactModel({
+    this.id = "",
+    required this.name,
+    required this.tel,
+  });
+
+  ContactModel.fromJson(Map json) {
+    id = json["id"];
+    name = json["name"];
+    tel = json["tel"];
+  }
+
+  Map toJson() {
+    Map json = {};
+    json["id"] = id;
+    json["name"] = name;
+    json["tel"] = tel;
+    return json;
+  }
+}
