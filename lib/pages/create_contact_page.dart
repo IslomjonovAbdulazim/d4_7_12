7d6fb6251064df9a9e161fc3a55bb09e77052891
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-02 18:31:16 +0500

    imp

--- lib/pages/create_contact_page.dart
+++ lib/pages/create_contact_page.dart
@@ -1,5 +1,8 @@
 import 'package:d4_7_12/models/contact_model.dart';
+import 'package:d4_7_12/service/network_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CreateContactPage extends StatefulWidget {
   final ContactModel? contact;
@@ -11,13 +14,71 @@ class CreateContactPage extends StatefulWidget {
 }
 
 class _CreateContactPageState extends State<CreateContactPage> {
+  final nameController = TextEditingController();
+  final nameFocus = FocusNode();
+  final telController = TextEditingController();
+  final telFocus = FocusNode();
+  bool isLoading = false;
+
   @override
   void initState() {
+    nameController.text = widget.contact?.name ?? "";
+    telController.text = widget.contact?.tel ?? "";
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameController,
+                focusNode: nameFocus,
+                decoration: InputDecoration(
+                  hintText: "Fullname...",
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: telController,
+                focusNode: telFocus,
+                decoration: InputDecoration(
+                  hintText: "Phone Number...",
+                ),
+              ),
+              SizedBox(height: 20),
+              CupertinoButton(
+                color: Colors.indigo,
+                onPressed: isLoading ? null : () async {
+                        isLoading = true;
+                        setState(() {});
+                        final contact = ContactModel(
+                          name: nameController.text,
+                          tel: telController.text,
+                        );
+                        if (widget.contact == null) {
+                          await NetworkService.create(contact);
+                        } else {
+                          contact.id = widget.contact!.id;
+                          await NetworkService.update(contact);
+                        }
+                        isLoading = false;
+                        setState(() {});
+                        Get.back();
+                      },
+                child: Center(
+                    child: isLoading
+                        ? CupertinoActivityIndicator()
+                        : Text("Save")),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
